b384a6020a42a9b14f27705b4375341351d0340b
Ariel-Vako <ariel.mardones@bourbaki.cl>
2019-06-03 20:55:25 -0400

    Archivo de chequeo de descarga. Se mejora la búsqueda de archivo.

---
+++ revisar.py
@@ -0,0 +1,17 @@
+__source__ = 'https://www.codementor.io/aviaryan/downloading-files-from-urls-in-python-77q3bs0un'
+
+import requests
+
+
+def is_downloadable(url):
+    """
+    Does the url contain a downloadable resource
+    """
+    h = requests.head(url, allow_redirects=True)
+    header = h.headers
+    content_type = header.get('content-type')
+    if 'text' in content_type.lower():
+        return False
+    if 'html' in content_type.lower():
+        return False
+    return True
