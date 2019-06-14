34f6417e5a888dc6856c0f8650fd73818b7169b0
Ariel-Vako <ariel.mardones@bourbaki.cl>
2019-06-14 09:53:20 -0400

    Se continua con el desarrollo de la descarga de addons.

--- addons.py
+++ addons.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os.path
 import easygui
 import time
+import revisar as rv
 
 print('\n')
 print('Ubique la dirección de archivo WoW.exe \n')
@@ -15,12 +16,13 @@ if os.path.isfile(wow) and os.path.isdir(addons_path):
     df_addons = pd.read_csv('Addons.csv')
     for url in df_addons['Url']:
         # Get the header first
-        h = requests.head(url, allow_redirects=True)
-        header = h.headers
-        # header.values() is a <class 'collections.abc.ValuesView'>
-        # header.items() is a  <class 'collections.abc.ItemsView'>
-        filepath = addons_path + '/'
-        filename = df_addons['Nombre'] + df_addons['Versión'] + header.get('Content-Encoding')
         r = requests.get(url, allow_redirects=True)
-        with open(filepath + filename, 'wb') as f:
-            f.write(r.content)
+        filename = rv.get_filename_from_cd(r.headers.get('content-disposition'))
+        if rv.is_downloadable(url):
+            h = requests.head(url, allow_redirects=True)
+            header = h.headers
+            filepath = addons_path + '/'
+            filename = df_addons['Nombre'] + df_addons['Versión'] + header.get('Content-Encoding')
+
+            with open(filepath + filename, 'wb') as f:
+                f.write(r.content)
--- revisar.py
+++ revisar.py
@@ -1,12 +1,15 @@
 __source__ = 'https://www.codementor.io/aviaryan/downloading-files-from-urls-in-python-77q3bs0un'
 
 import requests
+import re
 
 
 def is_downloadable(url):
     """
     Does the url contain a downloadable resource
     """
+    # header.values() is a <class 'collections.abc.ValuesView'>
+    # header.items() is a  <class 'collections.abc.ItemsView'>
     h = requests.head(url, allow_redirects=True)
     header = h.headers
     content_type = header.get('content-type')
@@ -15,3 +18,15 @@ def is_downloadable(url):
     if 'html' in content_type.lower():
         return False
     return True
+
+
+def get_filename_from_cd(cd):
+    """
+    Get filename from content-disposition
+    """
+    if not cd:
+        return None
+    fname = re.findall('filename=(.+)', cd)
+    if len(fname) == 0:
+        return None
+    return fname[0]
